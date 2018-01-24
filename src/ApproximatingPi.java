e7ca60a1f6e0931b1962ca375d2fb8de3ee8eab7
Mike Baldwin <mikebaldwin21@gmail.com>
2018-01-23 20:55:57 -0500

    Added a division count input.

--- src/ApproximatingPi.java
+++ src/ApproximatingPi.java
@@ -1,6 +1,33 @@
+import java.util.Scanner;
+
 public class ApproximatingPi {
-    public static void main(String[] args) {
-        double pi = 4.0 * (1.0 - 1.0 / 3.0 + 1.0 / 5.0 - 1.0 / 7.0 + 1.0 / 9.0 - 1.0 / 11.0 + 1.0 / 13.0);
-        System.out.println(pi);
+// Written by: Mike Baldwin
+// Calculates PI using the Gregory-Leibniz Series.
+
+public static void main(String[] args) {
+    Scanner inputStream = new Scanner(System.in);
+    double pi;
+    int divisionCount;
+    double tempVar;
+    double denominator;
+
+    System.out.print("Enter a number of divisions (The more divisions the closer the approximation): ");
+    divisionCount = inputStream.nextInt();
+
+    tempVar = 1.0;
+    denominator = 3.0;
+
+    for (int i = 0; i < divisionCount; i++) {
+        if ((i % 2) == 0) {
+            tempVar -= 1.0 / denominator;
+        } else {
+            tempVar += 1.0 / denominator;
+        }
+        denominator += 2.0;
     }
+
+    pi = 4.0 * tempVar;
+    System.out.println("The Java math library's Pi is: " + Math.PI);
+    System.out.println("You have approximated Pi as:   " + pi);
+}
 }
