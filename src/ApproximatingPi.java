adfe5321f95e339586546ee73e808669bdd754c5
Mike Baldwin <Michael.Baldwin3@rockets.utoledo.edu>
2018-02-06 22:39:33 -0500

    Changed user input and output from console to Swing.

--- src/ApproximatingPi.java
+++ src/ApproximatingPi.java
@@ -1,23 +1,41 @@
-import java.util.Scanner;
+import javax.swing.*;
 
 public class ApproximatingPi {
 // Written by: Mike Baldwin
 // Calculates PI using the Gregory-Leibniz Series.
 
 public static void main(String[] args) {
-    Scanner inputStream = new Scanner(System.in);
+    long divCount, time;
     double pi;
-    int divisionCount;
-    double tempVar;
-    double denominator;
+    String tempString;
 
-    System.out.print("Enter a number of divisions (The more divisions the closer the approximation): ");
-    divisionCount = inputStream.nextInt();
+    tempString = JOptionPane.showInputDialog(
+            null,
+            "Enter a number of divisions\n" +
+                    "(The more divisions the closer the approximation)",
+            "Approximating Pi",
+            JOptionPane.QUESTION_MESSAGE);
+    divCount = Long.parseLong(tempString);
+    time = System.currentTimeMillis();
+    pi = LeibnizMethod(divCount);
+    time = System.currentTimeMillis() - time;
+
+    JOptionPane.showMessageDialog(
+            null,
+            "The Java math library's Pi is: " + Math.PI + "\n" +
+                    "The GregoryLeibniz Series is: " + pi + "\n" +
+                    "It took " + time + " milliseconds to compute",
+            "Approximating Pi",
+            JOptionPane.INFORMATION_MESSAGE);
+}
+
+private static double LeibnizMethod(long divCount) {
+    double tempVar, denominator;
 
     tempVar = 1.0;
     denominator = 3.0;
 
-    for (int i = 0; i < divisionCount; i++) {
+    for (long i = 0; i < divCount; i++) {
         if ((i % 2) == 0) {
             tempVar -= 1.0 / denominator;
         } else {
@@ -26,8 +44,6 @@ public static void main(String[] args) {
         denominator += 2.0;
     }
 
-    pi = 4.0 * tempVar;
-    System.out.println("The Java math library's Pi is: " + Math.PI);
-    System.out.println("You have approximated Pi as:   " + pi);
+    return 4.0 * tempVar;
 }
 }
